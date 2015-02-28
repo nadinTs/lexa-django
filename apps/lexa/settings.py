193ab391d96e10c94d3368549a67d83fcfe0b215
Sergey Tsivin <sergey.tsivin@gmail.com>
2015-02-28 23:20:34 +0300

    Configured database connection

--- apps/lexa/settings.py
+++ apps/lexa/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'lexa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lexa',
+        'USER': 'lexa',
+        'PASSWORD': 'lexa',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -68,7 +72,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
