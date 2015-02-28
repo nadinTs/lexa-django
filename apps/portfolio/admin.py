a9ca92a63b2557192d35191762229392160bd828
Sergey Tsivin <sergey.tsivin@gmail.com>
2015-03-01 00:38:43 +0300

    Basic back office

--- apps/portfolio/admin.py
+++ apps/portfolio/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from portfolio.models import Category, Project, Artifact
 
-# Register your models here.
+
+class ArtifactInline(admin.StackedInline):
+    model = Artifact
+    extra = 3
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ArtifactInline]
+    list_filter = ['pub_date']
+    search_fields = ['title', 'lead']
+
+
+admin.site.register(Category)
+admin.site.register(Project, ProjectAdmin)
+
